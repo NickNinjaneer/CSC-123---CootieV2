0fc199792ec1d6e59f84bb23bd0d0143b4f63970
Nick Williams <Ninjaneer@Nicks-MacBook-Pro.local>
2014-09-19 22:39:52 -0500

    Initial Commit

---
+++ CootieV2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CootieV2
+//
+//  Created by Nick Williams on 9/19/14.
+//  Copyright (c) 2014 Ninjaneer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
